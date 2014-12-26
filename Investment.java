0bf84b26c04ff51419e6ce40e68c633cfc67cc02
li-chi <lichi321@gmail.com>
2014-12-26 16:00:40 +0800

    init commit

---
+++ Investment.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+
+public class Investment {
+	private Date purchaseDate;
+	private Date expireDate;
+	private double capital;
+	private double interestRate;
+	private int date;
+	private double dateInterest;
+	private double expectedReturn;
+	private double actualReturn;
+	
+	public Investment() {
+		
+	}
+	
+	public String toString() {
+		return null;
+	}
+}
